cd5de12247011653fd7c0e3c70bbd70b36b9603f
Marco Teti <sgttenore.gmail.com>
2019-05-17 14:06:53 -0700

    added inquirer npm

--- bamazon1.sql
+++ bamazon1.sql
@@ -4,15 +4,15 @@ CREATE DATABASE bamazon_DB;
 USE bamazon_DB;
 
 CREATE TABLE products(
-  id INT NOT NULL AUTO_INCREMENT,
-  item_name VARCHAR(50) NOT NULL,
-  department VARCHAR(45) NOT NULL,
-  price DECIMAL(7,2) NOT NULL,
-  stock INT default 1,
+  item_id INT AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(45) NOT NULL,
+  department_name VARCHAR(45) NOT NULL,
+  price DECIMAL(10,2) NOT NULL,
+  stock_quantity INT NOT NULL,
   PRIMARY KEY (id)
 );
 
-INSERT INTO products (item_name, department, price, stock)
+INSERT INTO products (product_name, department_anme, price, stock_quantity)
 VALUES ("Spirit Island", "Board-Games", 69.95, 10);
 
 INSERT INTO products (item_name, department, price, stock)
@@ -41,14 +41,3 @@ VALUES ("T'zolkin: The Mayan Temple", "Board-Games", 79.95, 10);
 
 INSERT INTO products (item_name, department, price, stock)
 VALUES ("Super Mario Maker 2", "Video-Games", 79.95, 10);
-
-
-
-
-
-
-
-
-
-
-
--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,5 +1,6 @@
 var mysql = require("mysql");
 var inquirer = require("inquirer");
+var Table = require('cli-table');
 
 // create the connection information for the sql database
 var connection = mysql.createConnection({
@@ -13,13 +14,120 @@ var connection = mysql.createConnection({
 
   // Your password
   password: "root",
-  database: "greatBay_DB"
+  database: "bamazon_DB"
 });
 
 // connect to the mysql server and sql database
-connection.connect(function(err) {
+connection.connect(function (err) {
   if (err) throw err;
   // run the start function after the connection is made to prompt the user
-  start();
+  getProducts();
 });
+
+function getProducts() {
+  console.log("Selecting all products...\n");
+  connection.query("SELECT * FROM products", function (err, res) {
+    if (err) throw err;
+    for (var i = 0; i < res.length; i++) {
+      console.log("Product ID: " + res[i].item_id + " || Product Name: " +
+        res[i].product_name + " || Price: " + res[i].price);
+    }
+    requestProduct();
+  });
+}
+
+var requestProduct = function () {
+  inquirer.prompt([{
+    name: "productID",
+    type: "input",
+    message: "Please enter product ID for product you want.",
+    validate: function (value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return false;
+    }
+  }, {
+    name: "productUnits",
+    type: "input",
+    message: "How many units do you want?",
+    validate: function (value) {
+      if (isNaN(value) === false) {
+        return true;
+      }
+      return false
+    }
+  }]).then(function (answer) {
+
+    // Queries database for selected product.
+    var query = "Select stock_quantity, price, department_name FROM products WHERE ?";
+    connection.query(query, { item_id: answer.productID }, function (err, res) {
+
+      if (err) throw err;
+
+      var available_stock = res[0].stock_quantity;
+      var price_per_unit = res[0].price;
+      var productDepartment = res[0].department_name;
+
+      // Checks there's enough inventory  to process user's request.
+      if (available_stock >= answer.productUnits) {
+
+        // Processes user's request passing in data to complete purchase.
+        completePurchase(available_stock, price_per_unit, productDepartment, answer.productID, answer.productUnits);
+      } else {
+
+        // Tells user there isn't enough stock left.
+        console.log("There isn't enough stock on hand! Products are on backorder...please check with us again");
+       
+        // Lets user request a new product.
+        requestProduct();
+      }
+    });
+  });
+};
+
+var completePurchase = function (availableStock, price, productDepartment, selectedProductID, productUnits) {
+
+  var updatedStock = availableStock - productUnits;
+  var totalPrice = price * productUnits;
+
+  var query = "UPDATE products SET ? WHERE ?";
+
+  connection.query(query, [{
+    stock_quantity: updatedStock
+  }, {
+    item_id: selectedProductID
+  }], function (err, res) {
+
+    if (err) throw err;
+    // Tells user purchase is a success.
+    console.log("Yay, your purchase is complete.");
+
+    // Display the total price for that purchase.
+    console.log("You're  payment has been received for the amount of : " + totalPrice);
+    inquirer
+      .prompt([
+        {
+          type: "list",
+          message: "Would you like to purchase more items?",
+          choices: ["Yes", "No"],
+          name: "action"
+        }
+      ])
+      .then(answers => {
+       if(answers.action === "Yes") {
+         getProducts();
+         requestProduct();
+      
+       } else {
+         connection.end();
+       }
+      });
+  });
+};
+
+
+
+
+
 
